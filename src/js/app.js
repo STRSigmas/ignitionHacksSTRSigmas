67c4ac25377eaac1167d1c4330a4c159c579ae8f
HOIman6321 <hoiman7074@gmail.com>
2025-08-15 21:52:17 -0400

    plan functions in app.js

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+// map initialization
+// address input handling
+// address autocomplete display
+// handle autocomplete selection
+// geocode selected address
+// start finding study locations
+// use current location
+// search for study locations
+// acquire specific location details
+// calculate distance
+// analyze study location reviews
+// calculate score for study location hours
+// calculate general score for study location
+// get color corresponding to score (range from red to green)
+// manage display of study locations in list and on map
+// display spots in list
+// display spots on map
+// pan to location on map when clicked
+// display detailed location information on map popup when clicked
